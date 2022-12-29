244041a3961811da167c483d53b0d56dbd5d9287
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-12-30 04:13:22 +0600

    style added

--- components/Results.js
+++ components/Results.js
@@ -4,7 +4,7 @@ import FlipMove from "react-flip-move"
 const Results = ({ results }) => {
     console.log(results)
     return (
-        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+        <FlipMove className="px-5 my-10 sm:grid gap-10 md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
             ))}
